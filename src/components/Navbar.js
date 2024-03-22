321cf869e0d51b31b48b40abfb7afffb539ee342
Aviral000 <avimalik02@gmail.com>
2024-03-22 19:41:15 +0530

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,63 @@
+// import React from 'react';
+// import './Navbar.css';
+// import InputAdornment from '@mui/material/InputAdornment';
+// import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+// export default function navbar() {
+//   return (
+//     <div className="navbar-container">
+//         <div>
+//             <div className="logo">Q tify</div>
+//             <img src="../assests/logo.png" alt="" />
+//         </div>
+//         <div className='search-box'>
+//             <div className="styled-input">
+//                 <input
+//                     id="outlined-basic"
+//                     type="text"
+//                     placeholder="Search an album of your choice"
+//                     className="styled-input-field"
+//                 />
+//                 <InputAdornment position="end">
+//                     <SearchOutlinedIcon className="search-icon" />
+//                 </InputAdornment>
+//             </div>
+//         </div>
+//         <div className="menu">
+//             <button className="menu-button">Give Feedback</button>
+//         </div>
+//     </div>
+//   )
+// }
+
+import React from 'react';
+import './Navbar.css';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+export default function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="logo-container">
+        <div className="logo">Q tify</div>
+        <img src="./assests/logo.png" alt="" />
+      </div>
+      <div className='search-box'>
+        <div className="styled-input">
+          <input
+            id="outlined-basic"
+            type="text"
+            placeholder="Search an album of your choice"
+            className="styled-input-field"
+          />
+          <InputAdornment position="end">
+            <SearchOutlinedIcon className="search-icon" />
+          </InputAdornment>
+        </div>
+      </div>
+      <div className="menu">
+        <button className="menu-button">Give Feedback</button>
+      </div>
+    </div>
+  )
+}
