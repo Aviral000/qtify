d611bd05e46416044f8f94989467da2c09716cf1
Aviral000 <avimalik02@gmail.com>
2024-03-23 19:45:09 +0530

    logo and name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,3 @@
-// import React from 'react';
-// import './Navbar.css';
-// import InputAdornment from '@mui/material/InputAdornment';
-// import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-
-// export default function navbar() {
-//   return (
-//     <div className="navbar-container">
-//         <div>
-//             <div className="logo">Q tify</div>
-//             <img src="../assests/logo.png" alt="" />
-//         </div>
-//         <div className='search-box'>
-//             <div className="styled-input">
-//                 <input
-//                     id="outlined-basic"
-//                     type="text"
-//                     placeholder="Search an album of your choice"
-//                     className="styled-input-field"
-//                 />
-//                 <InputAdornment position="end">
-//                     <SearchOutlinedIcon className="search-icon" />
-//                 </InputAdornment>
-//             </div>
-//         </div>
-//         <div className="menu">
-//             <button className="menu-button">Give Feedback</button>
-//         </div>
-//     </div>
-//   )
-// }
-
 import React from 'react';
 import './Navbar.css';
 import InputAdornment from '@mui/material/InputAdornment';
@@ -40,7 +8,7 @@ export default function Navbar() {
     <div className="navbar-container">
       <div className="logo-container">
         <div className="logo">Q tify</div>
-        <img src="./assests/logo.png" alt="" />
+        <img src="" alt="" />
       </div>
       <div className='search-box'>
         <div className="styled-input">
