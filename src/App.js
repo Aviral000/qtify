278e62891b58d51b7694fe4687c6d9d787b53aa4
Aviral000 <avimalik02@gmail.com>
2024-03-26 18:49:40 +0530

    songlist navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from "./components/Navbar";
+import Songlist from './components/Songlist';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Songlist />
     </div>
   );
 }
