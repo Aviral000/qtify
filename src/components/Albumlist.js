2a01f2db2c3923f31848766555a419b97aea2266
Aviral000 <avimalik02@gmail.com>
2024-03-28 19:08:41 +0530

    album3

--- src/components/Albumlist.js
+++ src/components/Albumlist.js
@@ -3,7 +3,6 @@ import "./Albumlist.css";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import axios from "axios";
 import "./grid1.styles.css";
 
@@ -11,6 +10,9 @@ export default function Albumlist() {
   const [albumlist, setAlbumlist] = useState([]);
   const [partialList, setPartialList] = useState([]);
   const [showAll, setShowAll] = useState(false);
+  const [newAlbumList, setNewAlbumList] = useState([]);
+  const [newPartialList, setNewPartialList] = useState([]);
+  const [showNewAll, setShowNewAll] = useState(false);
 
   const ApiCallForAlbums = async () => {
     try {
@@ -22,8 +24,19 @@ export default function Albumlist() {
     }
   };
 
+  const ApiCallForNewAlbums = async () => {
+    try {
+      const response = await axios.get("https://qtify-backend-labs.crio.do/albums/new");
+      setNewAlbumList(response.data);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
   useEffect(() => {
     ApiCallForAlbums();
+    ApiCallForNewAlbums();
   }, []);
 
   useEffect(() => {
@@ -39,41 +52,92 @@ export default function Albumlist() {
     }
   }, [showAll, albumlist]);
 
+  useEffect(() => {
+    const collapseNewList = () => {
+      const list = newAlbumList.slice(0, 6);
+      setNewPartialList(list);
+    };
+
+    if (!showNewAll) {
+      collapseNewList();
+    } else {
+      setNewPartialList(newAlbumList);
+    }
+  }, [showNewAll, newAlbumList]);
+
   const toggleShowAll = () => {
     setShowAll(!showAll);
+  };
+
+  const toggleShowNewAll = () => {
+    setShowNewAll(!showNewAll);
   };
 
   const texttoggler = () => {
     return showAll ? "Collapse" : "Show All";
   }
 
+  const newTexttoggler = () => {
+    return showNewAll ? "Collapse" : "Show All";
+  }
+
   return (
     <div style={{ margin: "1rem" }}>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div className='album-heading'>
-          Top Albums
-        </div>
+        <div className='album-heading'> Top Albums </div>
         <div style={{ marginRight: "2rem", color: "white" }}>
-          <Button onClick={toggleShowAll}>{texttoggler()}</Button>
+          <Button onClick={toggleShowAll} style={{ color: "white" }}>{texttoggler()}</Button>
         </div>
       </div>
       <div className="grid-container">
         {(showAll ? albumlist : partialList).map((album) => (
-          <div className="grid-item" key={album.id}>
-            <Card className="card">
-              <CardContent style={{ padding: 0 }}>
-                <div>
-                  <img src={album.image} alt={album.title} />
+          <div>
+            <div className="grid-item" key={album.id}>
+              <Card className="card">
+                <CardContent style={{ padding: 0 }}>
+                  <div>
+                    <img src={album.image} alt={album.title} />
+                  </div>
+                </CardContent>
+              </Card>
+              <div>
+                <div className='album-name'>
+                  {album.follows} Follows
                 </div>
-              </CardContent>
-            </Card>
-            <div>
-              <Typography variant="h5" component="h5">
-                <span>follows</span>: {album.follows}
-              </Typography>
-              <Typography variant="body2" component="p">
-                {album.title}
-              </Typography>
+              </div>
+            </div>
+            <div className='album-title'>
+              {album.title}
+            </div>
+          </div>
+        ))}
+      </div>
+      <hr color='#34C94B' />
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div className='album-heading'> New Albums </div>
+        <div style={{ marginRight: "2rem", color: "white" }}>
+          <Button onClick={toggleShowNewAll} style={{ color: "white" }}>{newTexttoggler()}</Button>
+        </div>
+      </div>
+      <div className="grid-container">
+        {(showNewAll ? newAlbumList : newPartialList).map((album) => (
+          <div>
+            <div className="grid-item" key={album.id}>
+              <Card className="card">
+                <CardContent style={{ padding: 0 }}>
+                  <div>
+                    <img src={album.image} alt={album.title} />
+                  </div>
+                </CardContent>
+              </Card>
+              <div>
+                <div className='album-name'>
+                  {album.follows} Follows
+                </div>
+              </div>
+            </div>
+            <div className='album-title'>
+              {album.title}
             </div>
           </div>
         ))}
